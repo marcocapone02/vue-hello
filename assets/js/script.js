e0bc2d51a99001aa984e849b7ba574f703a23aa7
Marco Capone <118698117+marcocapone02@users.noreply.github.com>
2023-04-19 23:53:12 +0200

    Last commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const {createApp} = Vue;
+createApp({
+  
+  data(){
+    
+    return{
+
+      message: "Hello Vue",
+      srcImg: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+
+    }
+  }
+
+}).mount("#app")
